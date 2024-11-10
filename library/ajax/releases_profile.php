c22563a21ef7dccefe528645bd91021a68510f2e
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-11-10 17:12:37 +0700

    Create releases_profile.php

---
+++ library/ajax/releases_profile.php
@@ -0,0 +1,45 @@
+<?php
+
+if (!defined('IN_AJAX')) die(basename(__FILE__));
+
+global $lang;
+
+if (!$user_id = intval($this->request['user_id']) or !$profiledata = get_userdata($user_id)) {
+	$this->ajax_die($lang['NO_USER_ID_SPECIFIED']);
+}
+
+if (!$mode = (string)$this->request['mode']) {
+	$this->ajax_die('invalid mode (empty)');
+}
+
+switch ($mode) {
+	case 'get_releases_profile':
+		$total_releases_size = $total_releases = $total_releases_completed = $total_releases_completed_full = 0;
+
+		$sql = "
+			SELECT COUNT(tor.poster_id), SUM(tor.size), SUM(tor.complete_count), SUM(ad.download_count)
+			FROM            " . BB_BT_TORRENTS . " tor
+				LEFT JOIN    " . BB_USERS . " u ON(u.user_id = tor.poster_id)
+				LEFT JOIN    " . BB_ATTACHMENTS_DESC . " ad ON(ad.attach_id = tor.attach_id)
+				LEFT JOIN    " . BB_BT_USERS . " ut ON(ut.user_id = tor.poster_id)
+			WHERE u.user_id = $user_id
+			GROUP BY tor.poster_id
+			LIMIT 1
+		";
+
+		if ($row = DB()->fetch_row($sql)) {
+			$total_releases = $row['COUNT(tor.poster_id)'];
+			$total_releases_size = $row['SUM(tor.size)'];
+			$total_releases_completed = $row['SUM(ad.download_count)'];
+			$total_releases_completed_full = $row['SUM(tor.complete_count)'];
+		}
+
+		$this->response['releases_profile_html'] = '[
+			' . $lang['RELEASES'] . ': <span class="seed bold">' . $total_releases . '</span> |
+			' . $lang['RELEASER_STAT_SIZE'] . ' <span class="seed bold">' . humn_size($total_releases_size) . '</span> |
+			' . $lang['DOWNLOADED'] . ': <span title="' . $lang['COMPLETED'] . ':&nbsp;' . declension((int)$total_releases_completed_full, 'times') . '" class="seed bold">' . declension((int)$total_releases_completed, 'times') . '</span> ]';
+		break;
+
+	default:
+		$this->ajax_die("invalid mode: $mode");
+}
